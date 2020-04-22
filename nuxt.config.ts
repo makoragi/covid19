fef632e2f44c8750443a8765aa5d74213def0745
makoragi <makoragi@gmail.com>
2020-04-23 07:11:02 +0900

    appid設定関連

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -58,6 +58,11 @@ const config: Configuration = {
         hid: 'twitter:image',
         name: 'twitter:image',
         content: 'https://dev-covid19-kumamoto.netlify.com/ogp.png'
+      },
+      {
+        hid: 'fb:app_id',
+        property: 'fb:app_id',
+        content: '234540354278042'
       }
     ],
     link: [
